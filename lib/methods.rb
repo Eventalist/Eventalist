0c9bcc33cb732053329336ad19d5146bcef887a7
TimoorKurdi <timoor.kurdi@gmail.com>
2014-10-10 16:14:56 -0400

    Edited seed, added server

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,17 +1,17 @@
-require "active record"
+require "active_record"
 
 class User < ActiveRecord::Base
 	self.has_many(:subscriptions)
 	self.has_many(:events)
-end 
+end
 
 class Event < ActiveRecord::Base
 end
 
 class Subscription < ActiveRecord::Base
-end 
+end
 
-class Category <ActiveRecord::Base
+class Category < ActiveRecord::Base
 		self.has_many(:events)
 		self.has_many(:subscriptions)
-end 
+end
