1df3c44b0b703664856fb886cffb23c16651efb6
TimoorKurdi <timoor.kurdi@gmail.com>
2014-10-10 14:50:24 -0400

    schema

---
+++ schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE eventalist;
+
+\c eventalist
+
+CREATE TABLE events (
+  id serial primary key,
+  category_id integer,
+  address varchar(255),
+  price varchar(255),
+  link text,
+  title varchar(255),
+  description text
+  );
+
+CREATE TABLE categories (
+  id serial primary key,
+  name varchar(255)
+  );
+
+CREATE TABLE subscriptions(
+ id serial primary key,
+ user_id integer,
+ category_id integer
+ );
+
+CREATE TABLE users (
+  id serial primary key,
+  name varchar(255),
+  email varchar(255)
+);
+
+
