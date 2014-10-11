af26096f38f4d9b438745c1f54c71163556f261d
tdshap <tess.shapiro@yahoo.com>
2014-10-11 14:23:13 -0400

    some changes

--- server.rb
+++ server.rb
@@ -9,7 +9,8 @@ require 'httparty'
 # require_relative './config/environments'
 # require_relative './lib/connection-yoshie'
 # require_relative './lib/connection-tim'
-require_relative './lib/connection-eric'
+# require_relative './lib/connection-eric'
+require_relative './lib/connection-tess'
 require_relative './lib/methods'
 
 after do
