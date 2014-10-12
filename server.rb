3aa1290efdd1160a82575d9bd98f9f33aff3524d
theerickramer <theerickramer@gmail.com>
2014-10-11 20:29:10 -0400

    pre subscriptions

--- server.rb
+++ server.rb
@@ -7,9 +7,9 @@ require 'sinatra/reloader'
 require 'pry'
 require 'httparty'
 # require_relative './config/environments'
-require_relative './lib/connection-tess'
+# require_relative './lib/connection-tess'
 # require_relative './lib/connection-yoshie'
-# require_relative './lib/connection-eric'
+require_relative './lib/connection-eric'
 require_relative './lib/methods'
 
 after do
@@ -147,6 +147,7 @@ get ("/users/:id/subscriptions") do
 
 end 
 
+binding.pry
 def subscription_params(params)
   params.slice(*Subscription.column_names)
 end
