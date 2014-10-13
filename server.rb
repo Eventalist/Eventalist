3602b7557bb4ba89074c327e5584af4a61bc073d
Yoshie <yoshiemuranaka@gmail.com>
2014-10-13 12:14:48 -0400

    merge conflict

--- server.rb
+++ server.rb
@@ -11,12 +11,12 @@ require "open-uri"
 # require_relative './config/environments'
 
 
-require_relative './lib/connection-tim'
+# require_relative './lib/connection-tim'
 
 # require_relative './lib/connection-eric'
 
 # require_relative './lib/connection-eric'
-# require_relative './lib/connection-yoshie'
+require_relative './lib/connection-yoshie'
 
 # require_relative './lib/connection-tess'
 
