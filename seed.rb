e384426422f46783a3503646cc7217f82ae22277
TimoorKurdi <timoor.kurdi@gmail.com>
2014-10-13 13:55:31 -0400

    added categories to seed file

--- seed.rb
+++ seed.rb
@@ -1,6 +1,10 @@
-require_relative('./lib/connection-yoshie')
+require 'active_record'
+
+require_relative('./lib/connection-tim')
 require_relative('./lib/methods')
 
 Category.create(name: 'music')
 Category.create(name: 'art')
 Category.create(name: 'theater')
+Category.create(name: 'free')
+Category.create(name: "nightlife")
