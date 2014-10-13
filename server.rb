423507867d9b4903c128140fa623818360c3c84c
tdshap <tess.shapiro@yahoo.com>
2014-10-13 12:14:22 -0400

    poop

--- server.rb
+++ server.rb
@@ -14,10 +14,6 @@ require_relative './lib/connection-tess'
 # require_relative './lib/connection-eric'
 # require_relative './lib/connection-yoshie'
 # require_relative './lib/connection-tim'
-
-# require_relative './lib/connection-eric'
-
-
 require_relative './lib/methods'
 
 after do
